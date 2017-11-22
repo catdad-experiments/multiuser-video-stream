6cd245159088844a0893e4dc9f709bc660fa3a1e
Kiril Vatev <vatev.1@gmail.com>
2017-11-21 22:07:16 -0500

    possibly adding hls
    nothing has worked so far, so I say possibly

--- stream.js
+++ stream.js
@@ -5,14 +5,16 @@ var through = require('through2');
 var binaryBits = `ffmpeg -f dshow`;
 var input = `-i video="Logitech HD Pro Webcam C920":audio="Microphone (HD Pro Webcam C920)"`;
 var codecs = `-vcodec libx264 -acodec libmp3lame`;
+//var codecs = `-vcodec libvpx -acodec libvorbis`;
 
 var opts1 = `-preset ultrafast -tune zerolatency -b 900k -f mpegts`;
-var opts2 = `-preset ultrafast -tune zerolatency -r 10 -async 1 -ab 24k -ar 22050 -bsf:v h264_mp4toannexb -maxrate 750k -bufsize 3000k -f mpegts`;
+var mpegts = `-preset ultrafast -tune zerolatency -r 10 -async 1 -ab 24k -ar 22050 -bsf:v h264_mp4toannexb -maxrate 750k -bufsize 3000k -f mpegts`;
+var hls = `-preset ultrafast -tune zerolatency -r 10 -crf 10 -b:v 1M -bufsize 3000k -f hls`;
 
 // note: add to the end of opts to flip a video
 var flip = `-vf hflip`;
 
-var command = `${binaryBits} ${input} ${codecs} ${opts2} -`;
+var command = `${binaryBits} ${input} ${codecs} ${hls} -`;
 
 var executable = command.split(' ')[0];
 var tokens = command.match(/\S+|"[^"]+"/g).slice(1);
